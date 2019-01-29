940578cc3f31eea29035e373f0c862b20d57878c
nikitabutkevich <44376155+nikitabutkevich@users.noreply.github.com>
2019-01-29 08:36:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 //Task 1
 console.log('Задание №1');
 
-function triangel(cathet1, cathet2, hypotenuse){
-	if ( hypotenuse ** 2 == cathet1 ** 2 + cathet2 ** 2) {
-		return true;
-	} else {
+function getTriangel(cathet1, cathet2, hypotenuse){
+	if ( hypotenuse ** 2 === cathet1 ** 2 + cathet2 ** 2) return true;
 		return false;
-	}
 }
-console.log(triangel(2, 2, 1));
+console.log(getTriangel(2, 0, 1));
 console.log(' ');
 
 
@@ -17,28 +14,14 @@ console.log('Задание №2');
 let str = prompt('Enter the string', '');
 let n = 2;
 
-function repeat(str) {
+function repeatString(str) {
 	for (i = 0; i < n; i++) {
 		str += str;
 		return str;
 	}
 }
 
-console.log(repeat(str, n));
-console.log('');
-
-
-//Task 3
-console.log('Задание №3');
-
-let people = +prompt('Введите число учащихся', '');
-let tabel = +prompt('Введите количество столов', '');
-
-function group(people, tabel) {
-	if (tabel < people / 2) return 'Столов меньше чем учащихся'
-		else return 'Столов хватает';
-}
-console.log(group(people, tabel));
+console.log(repeatString(str, n));
 console.log('');
 
 
@@ -47,7 +30,7 @@ console.log('');
 console.log('Задание №4');
 let point = +prompt('Введите оценку по 10-бальной системе', '');
 console.log('You enter ' + point);
-function rage(point) {
+function getStudentPoint(point) {
 	switch (point){
 		case 10: 
 			console.log('In Russian equivalent is  5+');
@@ -93,20 +76,20 @@ function rage(point) {
 			console.log('Это уже не 10-бальная система :(');
 	}
 }
-console.log(rage(point));
+console.log(getStudentPoint(point));
 console.log(' ');
 
 
 //Task 5
 
 console.log('Задание №5');
-function ziro() {
+function entretZero() {
 	do {
-		numberZiro = +prompt('Введите любое число ', '');
-	} while ( !(numberZiro == 0) );
+		numberZero = +prompt('Введите любое число ', '');
+	} while ( !(numberZero == 0) );
 }
 
-console.log(ziro());
+console.log(entretZero());
 console.log(' ');
 
 
@@ -133,17 +116,17 @@ console.log(' ');
 //Task 7
 
 console.log('Задание №7')
-let numberSimp = +prompt('Введите простое число', '');
+let simpleNumber = +prompt('Введите простое число', '');
 
-function simpleNumber(numberSimp) {
-	if (numberSimp == 2 || numberSimp == 3) return numberSimp + ' простое число';
+function getSimpleNumber(simpleNumber) {
+	if (simpleNumber == 2 || simpleNumber == 3) return simpleNumber + ' простое число';
 		
-		else if (numberSimp % 2 == 0 || numberSimp % 3 == 0) return numberSimp + ' составное число';
+		else if (simpleNumber % 2 == 0 || simpleNumber % 3 == 0) return simpleNumber + ' составное число';
 		
-		else return numberSimp + ' простое число';
+		else return simpleNumber + ' простое число';
 }
 
-console.log(simpleNumber(numberSimp));
+console.log(getSimpleNumber(simpleNumber));
 console.log(' ');
 
 
@@ -152,7 +135,7 @@ console.log(' ');
 console.log('Задание №8');
 let month = +prompt('Введите номер месяца', '');
 
-function season(month) {
+function getSeason(month) {
 	switch (month){
 		case 12:
 		case 1:
@@ -178,14 +161,14 @@ function season(month) {
 			console.log('Извините, но в году только 12 месяцев');
 	}
 }
-console.log(season(month));
+console.log(getSeason(month));
 console.log(' ');
 
 
 //Task 9
 console.log('Задание №9');
 
-function numberSeven() {
+function getNumberSeven() {
 
 	for (var i = 99; i > 10; i--) {
 		if (i % 7 == 0 ) {
@@ -198,7 +181,7 @@ function numberSeven() {
 	}
 }
 
-console.log(numberSeven());
+console.log(getNumberSeven());
 console.log(' ');
 
 
@@ -230,7 +213,7 @@ console.log('Задание №12');
 let unit = prompt('Введите единицу измерения: «Кб», «Мб», «Гб»', '');
 let size = +prompt('Введите размер в выбранном формате', '');
 
-function bite(unit, size) {
+function getBite(unit, size) {
 	switch (unit) {
 		case 'Кб':
 			console.log('В ' + size + ' Кб ' + size * 1024 + ' Байт');
@@ -246,7 +229,7 @@ function bite(unit, size) {
 	}
 }
 
-console.log(bite(unit, size));
+console.log(getBite(unit, size));
 console.log(' ');
 
 
@@ -254,12 +237,12 @@ console.log(' ');
 console.log('Задание №15');
 let numberPencil = +prompt('Введите количество карандашей', '');
 
-function pencil(numberPencil) {
+function pencilEnding(numberPencil) {
 	if ( numberPencil == 11 || numberPencil % 100 == 11) return 'Вы ввели ' + numberPencil + ' карандашЕЙ';
 	else if ( numberPencil % 10 == 1 ) return 'Вы ввели ' + numberPencil + ' карандаш';
 	else if ( numberPencil % 10 == 2 || numberPencil % 10 == 3 || numberPencil % 10 == 4) return 'Вы ввели ' + numberPencil + ' карандашА';
-	else return 'Вы ввели ' + numberPencil + ' карандашЕЙ';
+	return 'Вы ввели ' + numberPencil + ' карандашЕЙ';
 }
 
-console.log(pencil(numberPencil));
+console.log(pencilEnding(numberPencil));
 console.log(' ');
