6679f2b8e8f69b022774a2f6e5a54c8c653de697
nikitabutkevich <44376155+nikitabutkevich@users.noreply.github.com>
2019-02-09 08:10:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,57 @@ function getWeekDay(){
 console.log(getWeekDay());*/
 
 
-function getData(){
-	var userDate = prompt('Enter the date, month, year', '');
+//Task 9
+
+function getDate(){
+	var userDay = +prompt('Enter the day', '');
+	var userMonth = +prompt('Enter the month', '');
+	var userYear = +prompt('Enter the year', '');
+	var userEnterDate = new Date(userYear, userMonth-1, userDay+1);
+	
+	var numberWeekDay = userEnterDate.getDay();
+
+	/*if (numberWeekDay == 0 || numberWeekDay == 1) numberWeekDay = numberWeekDay;
+		else numberWeekDay;*/
+
+	switch (numberWeekDay){
+		case 0: 
+			alert('0Воскресенье');
+			break;
+		case 1: 
+			alert('1Понедельник');
+			break;
+		case 2: 
+			alert('2Вторник');
+			break;
+		case 3: 
+			alert('3Среда');
+			break;
+		case 4: 
+			alert('4Четверг');
+			break;
+		case 5:
+			alert('5Пятница');
+			break;
+		case 6: 
+			alert('6Суббота');
+			break;
+		default:
+    			alert('Нет такого дня недели');
+	}
 }
-console.log(getData());
+console.log(getDate());
+/*
+
+function getDateBirthday(){
+	var userDay = +prompt('Enter the day', '');
+	var userMonth = +prompt('Enter the month', '');
+	var userYear = +prompt('Enter the year', '');
+	var userEnterDateBirthday = new Date(userYear, userMonth-1, userDay+1);
+	console.log(userEnterDateBirthday);
+	var numberWeekDay = userEnterDateBirthday.getDay();
+
+	console.log(numberWeekDay);
+
+}
+console.log(getDateBirthday());*/
