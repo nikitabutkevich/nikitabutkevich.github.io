60e4782d71571ec55b658cb79d786f7b9f055611
nikitabutkevich <44376155+nikitabutkevich@users.noreply.github.com>
2019-04-21 23:52:45 +0300

    Update services.js

--- FinalProject/scr/services.js
+++ FinalProject/scr/services.js
@@ -22,7 +22,7 @@ class Services {
 	addNewPost (id, messages) {
 		 $.ajax({
 			url: 'https://api.vk.com/method/wall.post?&owner_id=' + id + '&message=' + messages + '&v=5.52&access_token=7b7476dd02f716d10ec5ef3f21c9fdf72f7d64b0f2f4ca60472419fe913530e494fa7196c77603faff4ee',
-			method: "POST",
+			method: "GET",
 			dataType: "JSONP",
 			error: function(error){
 				alert(error);
