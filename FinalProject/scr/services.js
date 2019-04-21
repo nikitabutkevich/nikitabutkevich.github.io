e8e0ccf192cd4f4b5a845d07543c7ca27bdb7ed2
nikitabutkevich <44376155+nikitabutkevich@users.noreply.github.com>
2019-04-22 00:11:47 +0300

    updates

--- FinalProject/scr/services.js
+++ FinalProject/scr/services.js
@@ -3,7 +3,7 @@
 class Services {
 	async getData(method, param){
 		let data = await $.ajax({
-			url: 'https://api.vk.com/method/' + method + '?&' + param + '&v=5.52&access_token=7b7476dd02f716d10ec5ef3f21c9fdf72f7d64b0f2f4ca60472419fe913530e494fa7196c77603faff4ee',
+			url: 'https://api.vk.com/method/' + method + '?&' + param + '&v=5.52&access_token=08fea5561cee2b3eac1e9bc94cb3f5e0d9b36b4fe36009e97ad7f5d60f1fc3d5709fedf00f101119bbc26',
 			method: "GET",
 			dataType: "JSONP"
 		});
@@ -12,7 +12,7 @@ class Services {
 
 	async getDataUserInfo(param, userId){
 		let data = await $.ajax({
-			url: 'https://api.vk.com/method/users.get?&' + param + userId + '&fields=online,followers_count,photo_200_orig,bdate,city,counters&v=5.52&access_token=7b7476dd02f716d10ec5ef3f21c9fdf72f7d64b0f2f4ca60472419fe913530e494fa7196c77603faff4ee',
+			url: 'https://api.vk.com/method/users.get?&' + param + userId + '&fields=online,followers_count,photo_200_orig,bdate,city,counters&v=5.52&access_token=08fea5561cee2b3eac1e9bc94cb3f5e0d9b36b4fe36009e97ad7f5d60f1fc3d5709fedf00f101119bbc26',
 			method: "GET",
 			dataType: "JSONP"
 		});
@@ -21,8 +21,8 @@ class Services {
 
 	addNewPost (id, messages) {
 		 $.ajax({
-			url: 'https://api.vk.com/method/wall.post?&owner_id=' + id + '&message=' + messages + '&v=5.52&access_token=7b7476dd02f716d10ec5ef3f21c9fdf72f7d64b0f2f4ca60472419fe913530e494fa7196c77603faff4ee',
-			method: "GET",
+			url: 'https://api.vk.com/method/wall.post?&owner_id=' + id + '&message=' + messages + '&v=5.52&access_token=08fea5561cee2b3eac1e9bc94cb3f5e0d9b36b4fe36009e97ad7f5d60f1fc3d5709fedf00f101119bbc26',
+			method: "POST",
 			dataType: "JSONP",
 			error: function(error){
 				alert(error);
