13d310deebc7e6998015aac21394f6cd37571bc1
nikitabutkevich <44376155+nikitabutkevich@users.noreply.github.com>
2019-04-24 21:14:23 +0300

    updates

--- FinalProject/scr/model.js
+++ FinalProject/scr/model.js
@@ -2,10 +2,10 @@
 
 class Model {
 	getAccessToken(){
-		var pageURL = window.location.href
-			pageURLData = pageURL.split('access_token=');
+		var pageURL = window.location.href,
+			pageURLData = pageURL.split('access_token='),
 			pageDataToken = pageURLData[1].split('&'),
-			accessToken = pageDataToken[0].
+			accessToken = pageDataToken[0]
 			;
 		console.log(accessToken);
 	}
