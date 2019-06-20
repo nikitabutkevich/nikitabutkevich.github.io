224b287c8940228121f53d56be8ff6c88e266cbb
nikitabutkevich <44376155+nikitabutkevich@users.noreply.github.com>
2019-06-20 23:12:56 +0300

    new project

---
+++ BeautySalon/src/js/index.js
@@ -0,0 +1,21 @@
+$('.block-make-up-school__course-programm__info').hide();
+
+function showClassProgram(){
+	$('.block-make-up-school__course-programm__info').show();
+}
+
+
+function hideClassProgram(){
+	$('.block-make-up-school__course-programm__info').hide();
+}
+
+$('.block-make-up-school__feedback').hide();
+
+function showFeedBackForm(){
+	$('.block-make-up-school__feedback').show();
+}
+
+
+function hideFeedBackForm(){
+	$('.block-make-up-school__feedback').hide();
+}
